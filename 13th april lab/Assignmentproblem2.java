0f443117cd92a96251852ab7e76265e70d2a6423
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-04-13 19:16:45 +0530

    Add files via upload

---
+++ 13th april lab/Assignmentproblem2.java
@@ -0,0 +1,21 @@
+package thurdylab;
+import java.util.*;
+
+public class Assignmentproblem2 {
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("Ritu");
+		al.add("Sana");
+		al.add("Megha");
+		al.add("nidhi");
+		al.add("rahul");
+		al.add("prit");
+		al.add("nidhi");
+		al.add("sumit");
+		al.add("madhuri");
+		al.add("nishu");
+		for(String s:al) {
+			System.out.println("The Name of Persons are: "+s);
+		}
+	}
+}
